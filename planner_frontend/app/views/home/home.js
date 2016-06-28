62f73807b3c70c46faf8f8f5f80b81347f207b0f
Netflux <netfluxer@gmail.com>
2016-06-28 19:10:15 +0800

    Added template for homepage

---
+++ planner_frontend/app/views/home/home.js
@@ -0,0 +1,7 @@
+// import required modules
+var ScrollView = require("ui/scroll-view").ScrollView;
+var StackLayout = require("ui/layouts/stack-layout").StackLayout;
+
+exports.loaded = function() {
+    
+}
