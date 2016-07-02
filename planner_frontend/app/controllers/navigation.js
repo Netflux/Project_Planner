ae89d3399d636c5ad905d36d42b74b6c4a08cf2f
Netflux <netfluxer@gmail.com>
2016-07-02 18:23:46 +0800

    Added new entries for navigation

--- planner_frontend/app/controllers/navigation.js
+++ planner_frontend/app/controllers/navigation.js
@@ -1,8 +1,36 @@
 // import required modules
 var frameModule = require("ui/frame");
 
+function toHomePage() {
+    frameModule.topmost().navigate("views/home/home");
+}
+
+function toNewEntry() {
+    var navigationData = {
+        moduleName: "views/new-entry/new-entry",
+        backStackVisible: false
+    };
+
+    frameModule.topmost().navigate(navigationData);
+}
+
+function toSettings() {
+    frameModule.topmost().navigate("views/settings/settings");
+}
+
+function toViewEntry(entryItem) {
+    var navigationData = {
+        moduleName: "views/view-entry/view-entry",
+        context: {entry: entryItem},
+        backStackVisible: false
+    };
+
+    frameModule.topmost().navigate(navigationData);
+}
+
 module.exports = {
-    toHomePage: function() {
-        frameModule.topmost().navigate("views/home/home");
-    }
+    toHomePage: toHomePage,
+    toNewEntry: toNewEntry,
+    toSettings: toSettings,
+    toViewEntry: toViewEntry
 }
